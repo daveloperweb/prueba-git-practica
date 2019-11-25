30ec6d3a286b0cc80778a200621bc820651de245
dbenitoroma <david.benito.romanillos@gmail.com>
2019-11-25 21:23:28 +0100

    añadidas diferencias sin console logs

---
+++ index.js
@@ -0,0 +1,20 @@
+var number = 5 // NO SE USA
+let string = "1"
+const boolean = true
+
+if (number === 5) {
+    var number = 10
+    let string = "5"
+    const boolean = false
+        // console.log("number dentro de if", number);
+        // console.log("string dentro de if", string);
+        // console.log("boolean dentro de if", boolean);
+}
+
+number = 10000
+string = "lo que sea"
+    // boolean = false   BUUUMMM
+
+// console.log("number fuera de if", number);
+// console.log("string fuera de if", string);
+// console.log("boolean fuera de if", boolean);
